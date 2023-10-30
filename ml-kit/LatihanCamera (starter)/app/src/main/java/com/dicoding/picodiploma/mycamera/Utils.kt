bf29f44b7ffc39da2343145fac40dbaa85ed80cd
Ahmad Arif Faizin <faizin.arief@gmail.com>
2023-10-30 15:39:26 +0700

    rename folder

---
+++ ml-kit/LatihanCamera (starter)/app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
@@ -0,0 +1,51 @@
+package com.dicoding.picodiploma.mycamera
+
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+private const val FILENAME_FORMAT = "yyyyMMdd_HHmmss"
+private val timeStamp: String = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(Date())
+
+fun getImageUri(context: Context): Uri {
+    var uri: Uri? = null
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, "$timeStamp.jpg")
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, "Pictures/MyCamera/")
+        }
+        uri = context.contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        )
+        // content://media/external/images/media/1000000062
+        // storage/emulated/0/Pictures/MyCamera/20230825_155303.jpg
+    }
+    return uri ?: getImageUriForPreQ(context)
+}
+
+private fun getImageUriForPreQ(context: Context): Uri {
+    val filesDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    val imageFile = File(filesDir, "/MyCamera/$timeStamp.jpg")
+    if (imageFile.parentFile?.exists() == false) imageFile.parentFile?.mkdir()
+    return FileProvider.getUriForFile(
+        context,
+        "${BuildConfig.APPLICATION_ID}.fileprovider",
+        imageFile
+    )
+    //content://com.dicoding.picodiploma.mycamera.fileprovider/my_images/MyCamera/20230825_133659.jpg
+}
+
+fun createCustomTempFile(context: Context): File {
+    val filesDir = context.externalCacheDir
+    return File.createTempFile(timeStamp, ".jpg", filesDir)
+}
