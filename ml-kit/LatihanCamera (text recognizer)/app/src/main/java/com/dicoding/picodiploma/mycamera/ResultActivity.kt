f66b6ea160c4a1b89560916ee8fe349d97156f85
Ahmad Arif Faizin <faizin.arief@gmail.com>
2023-11-01 10:46:07 +0700

    add starter

---
+++ ml-kit/LatihanCamera (text recognizer)/app/src/main/java/com/dicoding/picodiploma/mycamera/ResultActivity.kt
@@ -0,0 +1,38 @@
+package com.dicoding.picodiploma.mycamera
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.picodiploma.mycamera.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbar.title = "Hasil Analisis"
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        val imageUri = Uri.parse(intent.getStringExtra(EXTRA_IMAGE_URI))
+        imageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.resultImage.setImageURI(it)
+        }
+
+    }
+
+    companion object {
+        val EXTRA_IMAGE_URI = "key_image_uri"
+    }
+}
