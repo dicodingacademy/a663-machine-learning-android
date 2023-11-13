5a7f139d708371362a3d2f19f4c87dd8366e5ac5
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2023-11-13 11:40:34 +0700

    delete it for starter project
    ini dihilangkan agar menyesuaikan langkah di modul

--- ml-kit/LatihanCamera (starter)/app/src/main/java/com/dicoding/picodiploma/mycamera/ResultActivity.kt
+++ ml-kit/LatihanCamera (starter)/app/src/main/java/com/dicoding/picodiploma/mycamera/ResultActivity.kt
@@ -22,9 +22,6 @@ class ResultActivity : AppCompatActivity() {
             binding.resultImage.setImageURI(it)
         }
 
-        val detectedText = intent.getStringExtra(EXTRA_RESULT)
-        binding.resultText.text = detectedText
-
     }
 
     companion object {
