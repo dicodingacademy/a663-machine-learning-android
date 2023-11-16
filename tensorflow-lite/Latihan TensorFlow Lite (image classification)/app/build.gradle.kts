ad6f5e2024364c3f0b2f0054b44d3ed722a8ed96
Ahmad Arif Faizin <faizin.arief@gmail.com>
2023-11-16 13:58:58 +0700

    add tflite library

--- tensorflow-lite/Latihan TensorFlow Lite (image classification)/app/build.gradle.kts
+++ tensorflow-lite/Latihan TensorFlow Lite (image classification)/app/build.gradle.kts
@@ -49,4 +49,8 @@ dependencies {
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
+
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
 }
