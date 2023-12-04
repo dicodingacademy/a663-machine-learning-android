3a5963cd5222cdb95c7408a452c8b60d58e0536c
Ahmad Arif Faizin <faizin.arief@gmail.com>
2023-12-04 10:02:40 +0700

    add logic for inference

--- tensorflow-lite/Latihan TensorFlow Lite (prediction)/app/src/main/java/com/dicoding/latihantensorflowliteprediction/MainActivity.kt
+++ tensorflow-lite/Latihan TensorFlow Lite (prediction)/app/src/main/java/com/dicoding/latihantensorflowliteprediction/MainActivity.kt
@@ -2,10 +2,32 @@ package com.dicoding.latihantensorflowliteprediction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.dicoding.latihantensorflowliteprediction.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val predictionHelper = PredictionHelper(
+            context = this,
+            onResult = { result ->
+                binding.tvResult.text = result
+            },
+            onError = {errorMessage ->
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        )
+
+        binding.btnPredict.setOnClickListener {
+            val input = binding.edSales.text.toString()
+            predictionHelper.predict(input)
+        }
+
     }
 }
--- tensorflow-lite/Latihan TensorFlow Lite (prediction)/app/src/main/java/com/dicoding/latihantensorflowliteprediction/PredictionHelper.kt
+++ tensorflow-lite/Latihan TensorFlow Lite (prediction)/app/src/main/java/com/dicoding/latihantensorflowliteprediction/PredictionHelper.kt
@@ -1,21 +1,17 @@
 package com.dicoding.latihantensorflowliteprediction
 
 import android.content.Context
-import android.graphics.Movie
 import android.util.Log
 import com.google.android.gms.tflite.client.TfLiteInitializationOptions
 import com.google.android.gms.tflite.gpu.support.TfLiteGpu
 import com.google.android.gms.tflite.java.TfLite
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import org.tensorflow.lite.InterpreterApi
 import org.tensorflow.lite.gpu.GpuDelegateFactory
 import java.io.IOException
 import java.nio.ByteBuffer
-import java.util.HashMap
 
 class PredictionHelper(
-    val modelName: String = "rice_stock.tflite",
+    private val modelName: String = "rice_stock.tflite",
     val context: Context,
     private val onResult: (String) -> Unit,
     private val onError: (String) -> Unit,
@@ -38,17 +34,15 @@ class PredictionHelper(
     }
 
     private fun loadLocalModel() {
-//        return withContext(Dispatchers.IO) {
-            try {
-                val buffer: ByteBuffer = Utils.loadModelFile(
-                    context.assets, modelName
-                )
-                initializeInterpreter(buffer)
-                Log.v(TAG, "TFLite model loaded.")
-            } catch (ioException: IOException) {
-                ioException.printStackTrace()
-            }
-//        }
+        try {
+            val buffer: ByteBuffer = Utils.loadModelFile(
+                context.assets, modelName
+            )
+            initializeInterpreter(buffer)
+            Log.v(TAG, "TFLite model loaded.")
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+        }
     }
 
     private fun initializeInterpreter(buffer: ByteBuffer) {
@@ -64,17 +58,17 @@ class PredictionHelper(
         }
     }
 
-    fun predict(input: Int): Int {
-//        return withContext(Dispatchers.Default) {
-        val output = 0
+    fun predict(inputString: String) {
+        val inputArray = FloatArray(1)
+        inputArray[0] = inputString.toFloat()
+        val outputArray = Array(1) { FloatArray(1) }
         try {
-            interpreter?.run(input, output)
-        } catch (e: Exception){
+            interpreter?.run(inputArray, outputArray)
+            onResult(outputArray[0][0].toString())
+        } catch (e: Exception) {
             onError(context.getString(R.string.no_tflite_interpreter_loaded))
             Log.e(TAG, e.message.toString())
         }
-//        }
-        return output
     }
 
     companion object {
